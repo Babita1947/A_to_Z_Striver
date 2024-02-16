40467506a1d10f797dfb3c88d5a228cc2da1570c
Babita1947 <130829271+Babita1947@users.noreply.github.com>
2024-02-16 17:22:17 +0530

    added validate bst.cpp

---
+++ Binary_Tree/validate_BST.cpp
@@ -0,0 +1,33 @@
+//98. Validate Binary Search Tree
+// Definition for a binary tree node.
+
+#include<bits/stdc++.h>
+using namespace std;
+  struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  };
+class Solution {
+    public:
+  bool isValidBST(TreeNode* root) {
+    return isValidBST(root, nullptr, nullptr);
+  }
+
+ private:
+  bool isValidBST(TreeNode* root, TreeNode* minNode, TreeNode* maxNode) {
+    if (root == NULL)
+      return true;
+    if (minNode && root->val <= minNode->val)
+      return false;
+    if (maxNode && root->val >= maxNode->val)
+      return false;
+
+    return isValidBST(root->left, minNode, root) &&
+           isValidBST(root->right, root, maxNode);
+  }
+
+};
