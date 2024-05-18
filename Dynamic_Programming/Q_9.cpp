3751f9dba1e7dcdbe68fb15115e12f962c2b80ab
Babita1947 <130829271+Babita1947@users.noreply.github.com>
2024-05-18 19:22:25 +0530

    added Q_9.cpp and Q_10.cpp

---
+++ Dynamic_Programming/Q_9.cpp
@@ -0,0 +1,29 @@
+// 120. Triangle
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+    // space optimized
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int n = triangle.size();
+        vector<int> front(n,0);
+        
+        for(int j=0;j<n;j++){
+            front[j] = triangle[n-1][j];
+        }
+
+        for(int i=n-2;i>=0;i--){
+            vector<int> curr(n,0);
+            for(int j=i;j>=0;j--){
+                int down = triangle[i][j] + front[j]; 
+                int diagonal = triangle[i][j] + front[j+1];
+
+                curr[j] = min(down,diagonal);
+            }
+            front = curr;
+        }
+        return front[0];
+    }
+};
