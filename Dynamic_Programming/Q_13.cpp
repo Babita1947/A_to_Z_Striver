f1377c7f505babd0eebb225756233bf66b5e4d73
Babita1947 <130829271+Babita1947@users.noreply.github.com>
2024-05-21 18:56:28 +0530

    Added Q_13.cpp

---
+++ Dynamic_Programming/Q_13.cpp
@@ -0,0 +1,36 @@
+//416. Partition Equal Subset Sum
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+    bool subsetSumToK(int n,int k,vector<int> &nums){
+        vector<bool> prev(k+1,0),cur(k+1,0);
+        prev[0] = true,cur[0] = true;
+
+        for(int ind = 1;ind < n;ind++){
+            for(int target = 1;target <= k;target++){
+                bool notTake = prev[target];
+                bool take = false;
+
+                if(nums[ind] <= target)
+                   take = prev[target - nums[ind]];
+                cur[target] = take | notTake;
+            }
+            prev = cur;
+        }
+        return prev[k];
+    }
+public:
+    bool canPartition(vector<int>& nums) {
+        int n = nums.size();
+        int totSum = 0;
+        for(int i=0;i<n;i++){
+            totSum += nums[i];
+        }
+        if(totSum % 2 != 0)
+            return false;
+        int target = totSum / 2;
+        return subsetSumToK(n,target,nums);
+    }
+};
